1347ef8caa62cc94de205b4ba370612e2c0785fb
DarrenWu <deirenwu1101@gmail.com>
2022-12-23 23:44:57 +0800

    Basic setup for neo-tree

--- lua/darren/packer.lua
+++ lua/darren/packer.lua
@@ -51,5 +51,14 @@ return require('packer').startup(function(use)
 	  end
   })
 
+  use {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
 
 end)
