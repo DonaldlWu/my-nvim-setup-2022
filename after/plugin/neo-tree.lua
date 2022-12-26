9fc6f38fd9060c68acbe3672a30eeff450b8678d
DarrenWu <deirenwu1101@gmail.com>
2022-12-26 23:37:09 +0800

    Set neo-tree filesystem setting

--- after/plugin/neo-tree.lua
+++ after/plugin/neo-tree.lua
@@ -1,3 +1,20 @@
+require("neo-tree").setup({
+  close_if_last_window = true,
+  source_selector = {
+    winbar = true,
+    content_layout = "center",
+  },
+  filesystem = {
+    follow_current_file = true,
+    hijack_netrw_behavior = "open_current",
+    use_libuv_file_watcher = true,
+    window = { mappings = { h = "toggle_hidden" } },
+  },
+  event_handlers = {
+    { event = "neo_tree_buffer_enter", handler = function(_) vim.opt_local.signcolumn = "auto" end },
+  },
+})
+
 -- Key mapping
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>")
 vim.keymap.set("n", "<leader>o", "<cmd>Neotree focus<cr>")
