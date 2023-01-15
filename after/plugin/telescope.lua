0813f6b4235da62ac834cac68944ece0365f674c
DarrenWu <deirenwu1101@gmail.com>
2023-01-15 17:26:56 +0800

    Add new shortcut using telescope builtin

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,10 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pa',"<cmd>Telescope aerial<CR>" )
+vim.keymap.set('n', '<leader>pa',"<cmd>Telescope aerial<CR>")
+vim.keymap.set('n', '<leader>rf', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>gs', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>df', builtin.lsp_implementations, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
