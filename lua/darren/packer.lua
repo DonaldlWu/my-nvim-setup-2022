c0e41f03bb4125c0c7d92023254c681dfc09ea3b
DarrenWu <deirenwu1101@gmail.com>
2023-02-02 01:10:09 +0800

    Add vim-flog

--- lua/darren/packer.lua
+++ lua/darren/packer.lua
@@ -18,6 +18,7 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+  use('rbong/vim-flog')
   use("rebelot/kanagawa.nvim")
   use 'JoosepAlviste/palenightfall.nvim'
   use("vimwiki/vimwiki")
